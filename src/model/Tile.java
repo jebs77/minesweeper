9c7a7ce55700106c81cd505889d7ea8ac901b220
jens <jens.coosemans@student.kuleuven.be>
2022-03-09 16:22:14 +0100

    no message

---
+++ src/model/Tile.java
@@ -0,0 +1,44 @@
+package model;
+
+public class Tile extends AbstractTile{
+    private boolean open;
+    private boolean flagged;
+    private boolean explosive;
+
+    public Tile() {
+        open = false;
+        flagged = false;
+        explosive = false;
+    }
+
+    @Override
+    public boolean open() {
+        open = true;
+        return open;
+    }
+
+    @Override
+    public void flag() {
+        flagged = true;
+    }
+
+    @Override
+    public void unflag() {
+        flagged = false;
+    }
+
+    @Override
+    public boolean isFlagged() {
+        return flagged;
+    }
+
+    @Override
+    public boolean isExplosive() {
+        return explosive;
+    }
+
+    @Override
+    public boolean isOpened() {
+        return open;
+    }
+}
